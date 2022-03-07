5e521640109374f10f432e1a76d57aa4c7eb5810
eeliskeuda <eelis.pikkupeura@edu.keuda.fi>
2022-03-07 14:48:33 +0200

    Tehtävä 32 (kesken)
    Puuttuu vielä kirjaudu ulos napin piilottaminen sivun ensimmäistä kertaa avatessa.

---
+++ demo8/kirjautuminen.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", onkoKirjautunut);
+
+function onkoKirjautunut() {
+    if (localStorage.getItem("kirjautunut") === "kylla") {
+        document.getElementById("tervetuloa_teksti").textContent += localStorage.getItem("nimi");
+        document.getElementById("kirjautumis_lomake").style.display = "none";
+        document.getElementById("kirjaudu_ulos_nappi").style.display = "";
+    }
+}
+
+function kirjaudu() {
+    localStorage.setItem("nimi", document.getElementById("nimi").value);
+    localStorage.setItem("kirjautunut", "kylla");
+}
+
+function kirjauduUlos() {
+    localStorage.clear();
+    document.getElementById("kirjaudu_ulos_nappi").style.display = "none";
+    document.getElementById("kirjautumis_lomake").style.display = "";
+}
